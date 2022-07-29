5a8c604e88d2b52382b1b898b3a36a9eb61bebd6
guneyeroglu <benguneyeroglu@gmail.com>
2022-07-29 22:22:29 +0300

    added: icons added

---
+++ src/components/Icons/ArrowLogo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ArrowLogo = () => {
+  return <path d='m7 14 5-5 5 5z'></path>;
+};
+
+export default ArrowLogo;
--- src/components/Icons/Icon.js
+++ src/components/Icons/Icon.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import MainLogo from './MainLogo';
+import ArrowLogo from './ArrowLogo';
+import SettingsLogo from './SettingsLogo';
 
 const logos = {
   MainLogo: <MainLogo />,
+  ArrowLogo: <ArrowLogo />,
+  SettingsLogo: <SettingsLogo />,
 };
 
 const Icon = (props) => {
   const name = props.icon;
   const fill = props.fill;
+  const wh = props.wh ? props.wh : 24;
   return (
-    <svg width='40' height='40' viewBox='0 0 50 50' fill={fill ? fill : 'none'}>
+    <svg
+      width={wh}
+      height={wh}
+      viewBox={`0 0 ${wh} ${wh}`}
+      fill={fill ? fill : 'none'}
+    >
       {logos[name]}
     </svg>
   );
