0928d55a3ffeec16b01e853485bd04f6399df977
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-05 00:16:10 +0300

    updated: import i18n

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styles from './App.module.scss';
 import Home from './pages/Home/Home';
 
+import './assets/i18n/i18n';
+
 const App = () => {
   return (
     <div className={styles.container}>
