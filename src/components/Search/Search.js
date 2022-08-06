5e03c1bf317c35dd84d8c91ef91ac6607bdfdea6
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-06 18:36:47 +0300

    added: new comp + scss

---
+++ src/components/Search/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Icon from '../Icons/Icon';
+
+import styles from './Search.module.scss';
+
+const Search = (props) => {
+  return (
+    <div className={styles.search}>
+      <input
+        type={'text'}
+        value={props.a}
+        onChange={props.b}
+        placeholder={props.placeholder}
+      />
+      <div className={styles.search__icon}>
+        <Icon icon={'MagnifyingGlass'} fill='black' />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
