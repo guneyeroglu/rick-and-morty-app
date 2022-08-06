83918016145cd986349aacff91a6ecc831b5347d
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-06 18:35:37 +0300

    updated: home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,41 @@
 import React from 'react';
 
 import SideTitle from '../../components/SideTitle/SideTitle';
+import Search from '../../components/Search/Search';
+
+import { useTranslation } from 'react-i18next';
+
+import portalGun from '../../assets/images/portal-gun.png';
+import photo from '../../assets/images/r-m-photo.png';
 
 import styles from './Home.module.scss';
 
 const Home = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <SideTitle />
+      <div className={styles.container__search}>
+        <div className={styles.main}>
+          <div className={styles.main__title}>
+            <span>Rick & Morty</span>
+          </div>
+          <div className={styles.main__explanation}>
+            <span>{t('homeExplanation')}</span>
+          </div>
+          <Search placeholder={t('searchPlaceholder')} />
+        </div>
+        <div className={styles.body}>
+          <div className={styles.shadow}>
+            <img src={portalGun} alt='portal-gun' />
+          </div>
+        </div>
+      </div>
+      <div className={styles.container__picture}>
+        <div className={styles.shadow}>
+          <img src={photo} alt='rick-and-morty' />
+        </div>
+      </div>
     </div>
   );
 };
