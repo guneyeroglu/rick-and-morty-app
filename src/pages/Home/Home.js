c8ff5cdb90c193a043865a81a245b27e93729e17
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-06 13:39:57 +0300

    updated: home structure changed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
+
+import SideTitle from '../../components/SideTitle/SideTitle';
+
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <>
-      <Header />
-    </>
+    <div className={styles.container}>
+      <SideTitle />
+    </div>
   );
 };
 
