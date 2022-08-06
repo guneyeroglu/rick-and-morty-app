25b53a66885b11c4dc42da480fe5e16d85e5e413
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-06 13:38:32 +0300

    added: line-vertical

---
+++ src/components/LineVertical/LineVertical.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './LineVertical.module.scss';
+
+const LineVertical = () => {
+  return <div className={styles.line}></div>;
+};
+
+export default LineVertical;
