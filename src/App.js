4d8f701beb0ce67f4ac957eb29259dffe8bcf1f5
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-07 22:58:28 +0300

    updated: context added + structure changed + fav path added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import { Route, Routes } from 'react-router';
+
+import { DataProvider } from './store/Context/ContextFav';
 
 import Header from './components/Header/Header';
+
 import Home from './pages/Home/Home';
 import Characters from './pages/Characters/Characters';
-
-import './App.module.scss';
+import FavoriteChar from './pages/FavoriteChar/FavoriteChar';
 
 import './assets/i18n/index';
 
-import { Route, Routes } from 'react-router';
+import './App.module.scss';
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -17,21 +20,41 @@ const App = () => {
   };
 
   return (
-    <>
+    <DataProvider>
       <Header />
       <Routes>
         <Route
           path='/'
-          element={<Home value={searchValue} onSearch={handleSearchValue} />}
+          element={
+            <Home
+              value={searchValue}
+              setValue={setSearchValue}
+              onSearch={handleSearchValue}
+            />
+          }
         />
         <Route
           path='/characters'
           element={
-            <Characters value={searchValue} onSearch={handleSearchValue} />
+            <Characters
+              value={searchValue}
+              setValue={setSearchValue}
+              onSearch={handleSearchValue}
+            />
+          }
+        />
+        <Route
+          path='favoritechar'
+          element={
+            <FavoriteChar
+              value={searchValue}
+              setValue={setSearchValue}
+              onSearch={handleSearchValue}
+            />
           }
         />
       </Routes>
-    </>
+    </DataProvider>
   );
 };
 
