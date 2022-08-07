6a0a95400a1669cf4834e694d8c18c93d537f745
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-07 11:25:15 +0300

    updated: search value moved

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Characters from './pages/Characters/Characters';
@@ -9,12 +11,25 @@ import './assets/i18n/index';
 import { Route, Routes } from 'react-router';
 
 const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/characters' element={<Characters />} />
+        <Route
+          path='/'
+          element={<Home value={searchValue} onSearch={handleSearchValue} />}
+        />
+        <Route
+          path='/characters'
+          element={
+            <Characters value={searchValue} onSearch={handleSearchValue} />
+          }
+        />
       </Routes>
     </>
   );
