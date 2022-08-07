a8e978531f5daba29b47b6a36711bb97fdf9a634
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-07 23:04:09 +0300

    updated: remove useContext

--- src/pages/Characters/Characters.js
+++ src/pages/Characters/Characters.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Card from '../../components/Card/Card';
