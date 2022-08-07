0a56f1adb9af2c619a0d83e2d499b7031d857883
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-07 23:00:17 +0300

    updated: search comp changed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Icon from '../Icons/Icon';
 
 import { useTranslation } from 'react-i18next';
@@ -8,16 +9,25 @@ import styles from './Search.module.scss';
 const Search = (props) => {
   const { t } = useTranslation();
 
+  const { value, setValue, onSearch } = props;
+
   return (
     <div className={styles.search}>
       <input
         type={'text'}
-        value={props.value}
-        onChange={props.onSearch}
+        value={value}
+        onChange={onSearch}
         placeholder={t('searchPlaceholder')}
       />
       <div className={styles.search__icon}>
         <Icon icon={'MagnifyingGlassIcon'} fill='black' />
+      </div>
+      <div
+        className={styles.search__close}
+        style={{ display: value ? 'block' : 'none' }}
+        onClick={() => setValue('')}
+      >
+        <Icon icon={'CircleCloseIcon'} fill='black' />
       </div>
     </div>
   );
