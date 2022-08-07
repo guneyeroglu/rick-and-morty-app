5852249e1b2f1dc5867c2fe35e46493ddb079dde
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-07 11:25:53 +0300

    updated: icon changed

--- src/components/Icons/ArrowIcon.js
+++ src/components/Icons/ArrowIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ArrowIcon = () => {
-  return <path d='m7 14 5-5 5 5z'></path>;
+  return <path d='M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z'></path>;
 };
 
 export default ArrowIcon;
