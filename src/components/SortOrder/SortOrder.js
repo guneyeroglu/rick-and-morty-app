87456342d4172a285db35cf6631af6a304f8cf2f
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-08 22:47:47 +0300

    updated: sortOrder comp +scss upgrade

--- src/components/SortOrder/SortOrder.js
+++ src/components/SortOrder/SortOrder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Icon from '../../components/Icons/Icon';
 
@@ -6,13 +6,38 @@ import { useTranslation } from 'react-i18next';
 
 import styles from './SortOrder.module.scss';
 
-const SortOrder = () => {
+const SortOrder = (props) => {
   const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+
+  const handleSortDefault = () => {
+    setOpen(false);
+    props.onSort('default');
+  };
 
   return (
     <div className={styles.sort}>
-      <span>{t('sortText')}</span>
-      <Icon icon={'SortIcon'} fill='#3b3e43' />
+      <span onClick={handleSortDefault}>{t('sortText')}</span>
+      <div className={styles.sort__icon} onClick={() => setOpen(!open)}>
+        <Icon icon={'SortIcon'} fill='#3b3e43' />
+      </div>
+      <div
+        className={styles.sort__option}
+        style={{ display: open ? 'flex' : 'none' }}
+      >
+        <span
+          onClick={() => props.onSort('a-z')}
+          className={props.sort === 'a-z' ? styles.active : ''}
+        >
+          A &gt; Z
+        </span>
+        <span
+          onClick={() => props.onSort('z-a')}
+          className={props.sort === 'z-a' ? styles.active : ''}
+        >
+          Z &gt; A
+        </span>
+      </div>
     </div>
   );
 };
