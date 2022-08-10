d90dfa5cc9a85c90efeba5bc45c3faffbb370722
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-10 19:29:13 +0300

    updated: new term

--- src/assets/i18n/lang/en.js
+++ src/assets/i18n/lang/en.js
@@ -14,5 +14,6 @@ export const en = {
     locationDimension: 'Dimension',
     locationCreated: 'Created',
     locationAllChar: 'SEE ALL CHAR',
+    notFound: 'NOTFOUND',
   },
 };
--- src/assets/i18n/lang/tr.js
+++ src/assets/i18n/lang/tr.js
@@ -14,5 +14,6 @@ export const tr = {
     locationDimension: 'Boyut',
     locationCreated: 'Oluşturulma',
     locationAllChar: 'TÜM KARAKTERLERİ GÖR',
+    notFound: 'BULUNAMADI',
   },
 };
