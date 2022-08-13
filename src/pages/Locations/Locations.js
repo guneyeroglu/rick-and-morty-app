d0785249e4dbe77bb3a09df957aa02ffb2fc1fa5
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-13 20:07:53 +0300

    updated: hook changed and import comp

--- src/pages/Locations/Locations.js
+++ src/pages/Locations/Locations.js
@@ -4,12 +4,17 @@ import axios from 'axios';
 import LocationCard from '../../components/Cards/LocationCard/LocationCard';
 import Pagination from '../../components/Pagination/Pagination';
 
+import useSortOrder from '../../hooks/useSortOrder';
+
 import styles from './Locations.module.scss';
 
 const Locations = () => {
   const [locationData, setLocationData] = useState([]);
   const [pageData, setPageData] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortName, setSortName] = useState('name');
+
+  const { onSetSort, onSortOrder } = useSortOrder('default', sortName);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,10 +32,12 @@ const Locations = () => {
   return (
     <div className={styles.container}>
       <div className={styles.container__main}>
-        {locationData &&
-          locationData.map((location) => (
-            <LocationCard data={location} key={location.id} />
-          ))}
+        <LocationCard
+          data={locationData}
+          onSort={onSetSort}
+          onSortOrder={onSortOrder}
+          onSortName={setSortName}
+        />
       </div>
       <Pagination
         pageData={pageData}
