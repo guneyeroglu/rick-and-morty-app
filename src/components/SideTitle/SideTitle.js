201876fc8cfcae2585ba9c9c4a993e10a14ec33b
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-14 13:16:30 +0300

    updated: nav icons added

--- src/components/SideTitle/SideTitle.js
+++ src/components/SideTitle/SideTitle.js
@@ -3,6 +3,7 @@ import React from 'react';
 import SideTitleLineDot from './SideTitleLineDot';
 
 import styles from './SideTitle.module.scss';
+import Icon from '../Icons/Icon';
 
 const SideTitle = () => {
   return (
@@ -10,6 +11,30 @@ const SideTitle = () => {
       <SideTitleLineDot />
       <span className={styles.wrapper__title}>R & M</span>
       <SideTitleLineDot direction={'reverse'} />
+      <a
+        className={styles.wrapper__icon}
+        href='https://www.linkedin.com/in/guneyeroglu/'
+        target={'_blank'}
+        rel='noreferrer'
+      >
+        <Icon icon='LinkedInIcon' fill='#3b3e43' />
+      </a>
+      <a
+        className={styles.wrapper__icon}
+        href='https://github.com/guneyeroglu'
+        target={'_blank'}
+        rel='noreferrer'
+      >
+        <Icon icon='GitHubIcon' fill='#3b3e43' />
+      </a>
+      <a
+        className={styles.wrapper__icon}
+        href='https://rickandmortyapi.com/'
+        target={'_blank'}
+        rel='noreferrer'
+      >
+        <Icon icon='MainIcon' fill='#3b3e43' wh={50} />
+      </a>
     </div>
   );
 };
