6d991a8e244fab817f27041058cf0bb08a38348b
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-14 15:26:30 +0300

    updated: change for responsive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,34 +23,36 @@ const Home = (props) => {
   return (
     <div className={styles.container}>
       <SideTitle />
-      <div className={styles.container__search}>
-        <div className={styles.main}>
-          <div className={styles.main__title}>
-            <span>Rick & Morty</span>
+      <div className={styles.wrapper}>
+        <div className={styles.wrapper__search}>
+          <div className={styles.main}>
+            <div className={styles.main__title}>
+              <span>Rick & Morty</span>
+            </div>
+            <div className={styles.main__explanation}>
+              <span>{t('homeExplanation')}</span>
+            </div>
+            <Search value={props.value} onSetValue={props.onSetValue} />
+            <div className={styles.main__find}>
+              <div
+                className={styles.button}
+                style={{ display: props.value ? 'block' : 'none' }}
+                onClick={handleNavigateChar}
+              >
+                <Icon icon='ArrowIcon' fill='#3b3e43' />
+              </div>
+            </div>
           </div>
-          <div className={styles.main__explanation}>
-            <span>{t('homeExplanation')}</span>
-          </div>
-          <Search value={props.value} onSetValue={props.onSetValue} />
-          <div className={styles.main__find}>
-            <div
-              className={styles.button}
-              style={{ display: props.value ? 'block' : 'none' }}
-              onClick={handleNavigateChar}
-            >
-              <Icon icon='ArrowIcon' fill='#3b3e43' />
+          <div className={styles.body}>
+            <div className={styles.shadow}>
+              <img src={portalGun} alt='portal-gun' />
             </div>
           </div>
         </div>
-        <div className={styles.body}>
+        <div className={styles.wrapper__picture}>
           <div className={styles.shadow}>
-            <img src={portalGun} alt='portal-gun' />
+            <img src={photo} alt='rick-and-morty' />
           </div>
-        </div>
-      </div>
-      <div className={styles.container__picture}>
-        <div className={styles.shadow}>
-          <img src={photo} alt='rick-and-morty' />
         </div>
       </div>
     </div>
