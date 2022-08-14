a95ffee083f81ac7a588205f528af854bd409487
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-14 10:29:26 +0300

    updated: comp structure changed

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,32 +1,18 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
+import NoData from '../../components/NoData/NoData';
 
 import { useTranslation } from 'react-i18next';
-
-import notFound from '../../assets/images/r-m-portal.png';
 
 import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
-
-  const handleNavigate = () => {
-    navigate('/');
-  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.container__nav} onClick={handleNavigate}>
-        <span>{t('homeNav')}</span>
-      </div>
-      <div className={styles.container__image}>
-        <img src={notFound} alt='no-match' />
-      </div>
+    <NoData url='/' text={'homeNav'}>
       <div className={styles.container__content}>
         <span>404 - Wubba Lubba Dub Dub - {t('notFound')}</span>
       </div>
-    </div>
+    </NoData>
   );
 };
 
